dbccc3a7370924b91e57373967a382eb43f37564
lajiang <lajiang@2dfire.com>
2018-11-14 14:53:34 +0800

    new lib

--- app/src/main/java/com/lijing/dev/arch/MainActivity.java
+++ app/src/main/java/com/lijing/dev/arch/MainActivity.java
@@ -2,19 +2,11 @@ package com.lijing.dev.arch;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-
-import com.lijing.dev.logutil.DebugLogger;
-import com.lijing.dev.utils.ContextUtils;
-import com.lijing.dev.utils.FileUtils;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        ContextUtils.setContext(this);
-        setContentView(R.layout.activity_main);
-
     }
 }
