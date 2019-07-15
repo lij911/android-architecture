9273fa0075b4269616e6dd8e211eaa121b104afa
lajiang <lajiang@2dfire.com>
2019-07-15 14:40:38 +0800

    【ADD】android 动画

--- app-module/todo/src/main/java/com/lijing/dev/todo/MainActivity.kt
+++ app-module/todo/src/main/java/com/lijing/dev/todo/MainActivity.kt
@@ -1,15 +1,14 @@
 package com.lijing.dev.todo
 
+import android.animation.ObjectAnimator
 import android.support.v7.widget.Toolbar
-import android.widget.Toast
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import com.alibaba.android.arouter.facade.annotation.Route
-import com.bumptech.glide.Glide
 import com.lijing.dev.core.RouterConstants
 import com.lijing.dev.mvvm.core.ToolBarActivity
 import com.lijing.dev.mvvm.di.AppComponent
 import com.lijing.dev.todo.di.DaggerTodoComponent
-import com.lijing.dev.utils.BitmapUtils
-import com.lijing.dev.utils.DensityUtil
 import kotlinx.android.synthetic.main.activity_main.*
 import javax.inject.Inject
 
@@ -39,12 +38,33 @@ class MainActivity : ToolBarActivity<MainViewModel>() {
     }
 
     override fun initViewsAndEvents() {
+        // 属性动画
+        tv_subTitle.setOnClickListener {
+            ObjectAnimator.ofFloat(it, "rotationY", 0f, 360f)
+                    .setDuration(2000)
+                    .start()
+        }
+        // View 动画
+        tv_title.setOnClickListener {
+            val animation = AnimationUtils.loadAnimation(applicationContext, R.anim.res_view_anim_alpha)
+            // 设置动画监听
+            animation.setAnimationListener(object : Animation.AnimationListener {
+                override fun onAnimationRepeat(animation: Animation?) {
+                }
 
+                override fun onAnimationEnd(animation: Animation?) {
+                }
+
+                override fun onAnimationStart(animation: Animation?) {
+                }
+
+            })
+            it.startAnimation(animation)
+        }
     }
 
     override fun setupToolBar(toolbar: Toolbar) {
         initStatusBar(R.color.res_black)
-
     }
 
 }
